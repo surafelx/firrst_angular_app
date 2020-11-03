5bafb0a105028bc90bce71edb90a1729974677ee
surafelx <surafelyimam.sy@gmail.com>
2020-11-03 16:10:08 +0300

    Added values using String Interpolation

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -5,5 +5,9 @@ import { Component } from '@angular/core';
     templateUrl: './server.component.html'
 })
 export class ServerComponent {
-    
+    serverId: number = 10;
+    serverStatus: string ='Active';
+    getServerStatus() {
+        return this.serverStatus;
+    }
 }
